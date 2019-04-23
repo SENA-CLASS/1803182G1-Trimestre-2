c33b1d48ddd945122b580b189c4342bd6eebd7e2
Hernando Enrique Moreno Moreno <hemoreno@misena.edu.co>
2019-04-23 08:21:10 -0500

    chore: ejemplo de como leer en java

---
+++ java/holamundo/src/main/java/co/edu/sena/holamundo/Leer.java
@@ -0,0 +1,18 @@
+package co.edu.sena.holamundo;
+
+import java.util.Scanner;
+
+public class Leer {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		String a = teclado.nextLine();
+		
+		System.out.println(a);
+		
+		
+
+	}
+
+}
