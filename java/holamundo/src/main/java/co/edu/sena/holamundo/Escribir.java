d7d6712b13d7cbb00d3ed8aace0f44400586315e
Hernando Enrique Moreno Moreno <hemoreno@misena.edu.co>
2019-04-23 08:14:27 -0500

    chore: escribir versión java

---
+++ java/holamundo/src/main/java/co/edu/sena/holamundo/Escribir.java
@@ -0,0 +1,14 @@
+package co.edu.sena.holamundo;
+
+import java.util.logging.Logger;
+
+public class Escribir {
+	
+	private static final Logger LOG = Logger.getLogger(Escribir.class.getName());
+
+	public static void main(String[] args) {
+		LOG.info("hola estoy escribiendo pero en log");
+
+	}
+
+}
--- java/holamundo/src/main/java/co/edu/sena/holamundo/HolaMundo.java
+++ java/holamundo/src/main/java/co/edu/sena/holamundo/HolaMundo.java
@@ -1,10 +1,18 @@
 package co.edu.sena.holamundo;
 
+import java.util.logging.Logger;
+
 public class HolaMundo {
+	
+	private static final Logger LOG = Logger.getLogger(HolaMundo.class.getName());
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo de java");
-
+		System.out.println("hola mundo"); // escibir en la consola hace un salto linea
+		System.out.print("segunda linea");
+		System.out.print("tercar linea");
+		System.out.println("asdfasdf"+"asdfadfsf");// concatenar
+		
+		LOG.info("hola mundo");
 	}
 
 }
