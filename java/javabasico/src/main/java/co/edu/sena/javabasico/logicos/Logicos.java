ecdc779d20d4c22839da06f69d82c8cb1c42fec7
Hernando Enrique Moreno Moreno <hemoreno@misena.edu.co>
2019-04-23 10:21:14 -0500

    chore: operadores relacionales y logicos

---
+++ java/javabasico/src/main/java/co/edu/sena/javabasico/logicos/Logicos.java
@@ -0,0 +1,12 @@
+package co.edu.sena.javabasico.logicos;
+
+public class Logicos {
+	public static void main(String ... args) {
+		// operadores relacionales
+		
+		
+		
+		System.out.println(5>=2);
+	}
+
+}
