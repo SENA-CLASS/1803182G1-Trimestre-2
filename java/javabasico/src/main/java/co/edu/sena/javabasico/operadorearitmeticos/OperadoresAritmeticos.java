3a8b251af8d300db6c5356b0b8d9c6806a6bb57f
Hernando Enrique Moreno Moreno <hemoreno@misena.edu.co>
2019-04-23 10:14:13 -0500

    chore: operadores aritmeticos

---
+++ java/javabasico/src/main/java/co/edu/sena/javabasico/operadorearitmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,29 @@
+package co.edu.sena.javabasico.operadorearitmeticos;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		// suma
+		
+		int a = 5+3;
+		int y = 9-4;
+		int u = 5*9;
+		int k = 8/5;
+		int r = 5 % 2;
+		System.out.println(r);
+		int e = (int)7/2;// trunc
+		
+		int i=0;
+		i++; // i=i+1;
+		++i; // i=i+1;
+		
+		int rr=1;
+		System.out.println(rr++);
+		
+		
+		
+		
+
+	}
+
+}
