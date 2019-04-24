553cec3b752bebbd8a7ff6f9abd779b3d53785ba
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2019-04-24 08:16:38 -0500

    chore: ejemplo de un debug

---
+++ java/javabasico/src/main/java/co/edu/sena/javabasico/debug/EjemploDebug.java
@@ -0,0 +1,23 @@
+package co.edu.sena.javabasico.debug;
+
+public class EjemploDebug {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=4;
+		int c=7;
+		int d=9;
+		
+		a=a+b+c;
+		b=a*3+2;
+		c=c+a+d;
+		d=b+d;
+		c=c+a+b+d;
+		
+		
+		
+		
+
+	}
+
+}
