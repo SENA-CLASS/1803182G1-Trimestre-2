238e14bf9885b861910d862e85d993d443d1f85e
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2019-04-24 09:33:15 -0500

    chore: constantes en java

---
+++ java/javabasico/src/main/java/co/edu/sena/javabasico/constantes/Constantes.java
@@ -0,0 +1,11 @@
+package co.edu.sena.javabasico.constantes;
+
+public class Constantes {
+
+	public static void main(String[] args) {
+		final double PI = 3.14151616161616; // snake_uppercase
+		final double VELOCIDAD_LUZ= 299792458;
+		final double GRAVEDAD_TIERRA = 9.8;
+	}
+
+}
