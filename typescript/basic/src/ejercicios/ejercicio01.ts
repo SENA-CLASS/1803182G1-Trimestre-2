e431e5c8ab759e421acf55a44bc01bb42f688de5
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2019-04-26 08:15:22 -0500

    chore: concatenar y constantes

--- typescript/basic/src/ejercicios/ejercicio01.ts
+++ typescript/basic/src/ejercicios/ejercicio01.ts
@@ -1,3 +1,10 @@
 let var1 : number;
 
-var1=34;
+const NOMBRE_CONSTANTE: number =4354;
+
+
+let texto1 : string = "hola";
+let texto2 : string = "mundo";
+
+let texto3 : string = texto1+texto2; // esto si es concatenar
+console.log(texto1," ",texto2);// esto no es concatenar
