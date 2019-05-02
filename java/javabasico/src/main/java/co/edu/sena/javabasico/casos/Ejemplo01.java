ed87ac087d451b8fa25f969021df5cb99aaa456d
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2019-05-02 07:05:00 -0500

    chore: switch java

---
+++ java/javabasico/src/main/java/co/edu/sena/javabasico/casos/Ejemplo01.java
@@ -0,0 +1,30 @@
+package co.edu.sena.javabasico.casos;
+
+import java.util.Scanner;
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        int a;
+        Scanner teclado = new Scanner(System.in);
+        a = teclado.nextInt();
+
+        switch (a){
+            case 1:{
+                System.out.println("uno");
+                break;
+            }
+            case 2:{
+                System.out.println("dos");
+                break;
+            }
+            case 3:{
+                System.out.println("tres");
+                break;
+            }
+            default: {
+                System.out.println("no conozco ese numero");
+            }
+        }
+
+    }
+}
---
+++ java/javabasico/src/main/java/co/edu/sena/javabasico/casos/Ejemplo02.java
@@ -0,0 +1,33 @@
+package co.edu.sena.javabasico.casos;
+
+import java.util.Scanner;
+
+public class Ejemplo02 {
+    public static void main(String[] args) {
+        String b;
+        Scanner teclado = new Scanner(System.in);
+        b = teclado.next();
+
+        switch (b){
+            case "uno":
+            case "UNO":{
+                System.out.println(1);
+                break;
+            }
+            case "dos":
+            case "DOS":{
+                System.out.println(2);
+                break;
+            }
+            case "tres":
+            case "TRES":{
+                System.out.println(3);
+                break;
+            }
+            default: {
+                System.out.println("no conozco ese numero");
+            }
+        }
+
+    }
+}
