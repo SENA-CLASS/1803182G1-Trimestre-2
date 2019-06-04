c4d9bf01e810d47cb8920202715f16398f90d9de
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2019-06-04 08:02:28 -0500

    chore: inner class into methods

---
+++ poo/ejemplos/src/main/java/co/edu/sena/poo/ejemplo15clasesanidadas/metodos/Ejemplo01.java
@@ -0,0 +1,13 @@
+package co.edu.sena.poo.ejemplo15clasesanidadas.metodos;
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        // clase que esta dentro de un metodo se llama inner class into methods
+        class Mesa{
+
+        }
+
+        Mesa mesa = new Mesa();
+        System.out.println(mesa);
+    }
+}
